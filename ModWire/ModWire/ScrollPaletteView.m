605b8f10f795b148715b087ebbf598b05003ca3e
jnoble <jessica.noble11@gmail.com>
2012-03-20 11:15:25 -0400

    editor, scroll palette, and icon control palettes created

---
+++ ModWire/ModWire/ScrollPaletteView.m
@@ -0,0 +1,13 @@
+//
+//  ScrollPaletteView.m
+//  ModWire
+//
+//  Created by Jessica Noble on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ScrollPaletteView.h"
+
+@implementation ScrollPaletteView
+
+@end
