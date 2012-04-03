479de5bfd79415649d56ec974c00df99bb03e4ed
tuckstone <tuckstone@gmail.com>
2012-04-02 20:34:05 -0400

    Added DraggableIcon.h / .m.  Right now, its just a class that inherits from UIView that can be moved around, might be a good basis for the movable icons.

---
+++ ModWire/ModWire/DraggableIcon.h
@@ -0,0 +1,15 @@
+//
+//  DraggableIcon.h
+//  ModWire
+//
+//  Created by Lion User on 02/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DraggableIcon : UIView
+
+@property (nonatomic) CGPoint startPoint;
+
+@end
---
+++ ModWire/ModWire/DraggableIcon.m
@@ -0,0 +1,46 @@
+//
+//  DraggableIcon.m
+//  ModWire
+//
+//  Created by Lion User on 02/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DraggableIcon.h"
+
+@implementation DraggableIcon
+@synthesize startPoint;
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    startPoint = [[touches anyObject] locationInView:self];
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    CGPoint movePoint = [[touches anyObject] locationInView:self.superview];
+    movePoint.x -= startPoint.x;
+    movePoint.y -= startPoint.y;
+    CGRect movFrame = [self frame];
+    movFrame.origin = movePoint;
+    [self setFrame:movFrame];
+    
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
